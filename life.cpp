920e601b31fcf2e54ccb298494a9ce9d4783e85a
jakubKuznik <kuznja16@gmail.com>
2024-04-23 21:47:14 +0200

    copy values

--- life.cpp
+++ life.cpp
@@ -26,6 +26,9 @@ using namespace std;
 #define COLUMN_SIZE 1
 #define ROWS 2
 
+#define FROM_DOWN 1
+#define FROM_UP 2
+
 /**
  * First process that parses input into the globalTable and sets the number 
  * of steps 
@@ -80,8 +83,6 @@ void first_proces(int argc, char *argv[], vector<int> *global_table,
 		}	
 	}
 
-
-
 	// close the file 
 	file.close();
 
@@ -133,6 +134,49 @@ void game_of_life(vector<int> *local, int game_info[3], int rank, int size){
 	// W  C  E
 	// SW S SE
 
+	// neigh-up
+	// neigh-down
+	vector<int> from_up(game_info[COLUMN_SIZE]);
+	vector<int> from_down(game_info[COLUMN_SIZE]);
+	
+	vector<int> my_first_row(game_info[COLUMN_SIZE]);
+	vector<int> my_last_row(game_info[COLUMN_SIZE]);
+
+	MPI_Status status;
+	MPI_Request req[2];
+	
+	int j = local->size() - game_info[COLUMN_SIZE];
+	for (int i = 0; i < game_info[COLUMN_SIZE]; i++){
+		my_first_row[i] = (*local)[i];
+		my_last_row[i]  = (*local)[j++];
+	}
+
+	// send first and last row to each of my neighbour 
+	if (rank == MAIN_PROCES){ // first process 
+		MPI_Isend(my_first_row.data(), game_info[COLUMN_SIZE], MPI_INT, (size-1), FROM_DOWN, MPI_COMM_WORLD, &req[0]);
+		MPI_Isend(my_last_row.data(), game_info[COLUMN_SIZE], MPI_INT, (rank+1), FROM_UP,   MPI_COMM_WORLD, &req[1]);
+		
+		MPI_Recv(from_down.data(), game_info[COLUMN_SIZE], MPI_INT, (rank+1), FROM_DOWN, MPI_COMM_WORLD, &status);
+		MPI_Recv(from_up.data(), game_info[COLUMN_SIZE], MPI_INT, (size-1), FROM_UP,   MPI_COMM_WORLD, &status);
+	}
+	else if (rank == size-1){ // last process 
+		MPI_Isend(my_first_row.data(), game_info[COLUMN_SIZE], MPI_INT, (rank-1), FROM_DOWN,  MPI_COMM_WORLD, &req[0]);
+		MPI_Isend(my_last_row.data(), game_info[COLUMN_SIZE], MPI_INT, MAIN_PROCES, FROM_UP, MPI_COMM_WORLD, &req[1]);
+		
+		MPI_Recv(from_down.data(), game_info[COLUMN_SIZE], MPI_INT, MAIN_PROCES, FROM_DOWN, MPI_COMM_WORLD, &status);
+		MPI_Recv(from_up.data(), game_info[COLUMN_SIZE], MPI_INT, (rank-1), FROM_UP,      MPI_COMM_WORLD, &status);
+	}
+	else{
+		MPI_Isend(my_first_row.data(), game_info[COLUMN_SIZE], MPI_INT, (rank-1), FROM_DOWN, MPI_COMM_WORLD, &req[0]);
+		MPI_Isend(my_last_row.data(), game_info[COLUMN_SIZE], MPI_INT, (rank+1), FROM_UP,   MPI_COMM_WORLD, &req[1]);
+		
+		MPI_Recv(from_down.data(), game_info[COLUMN_SIZE], MPI_INT, (rank+1), FROM_DOWN,    MPI_COMM_WORLD, &status);
+		MPI_Recv(from_up.data(), game_info[COLUMN_SIZE], MPI_INT, (rank-1), FROM_UP,      MPI_COMM_WORLD, &status);
+	}
+
+	// if 0 -> use neig-up 
+	int curr_row = 0;
+
 
 
 }
@@ -161,7 +205,15 @@ int main(int argc, char *argv[]) {
 		cout << "Column size:  " << game_info[COLUMN_SIZE] << endl;
 		cout << "Rows:         " << game_info[ROWS] << endl;
 		cout << "Steps:        " << game_info[STEPS] << endl;
-
+		
+		cout << endl << "START: " << endl;
+		for (int i=0; i < global_table.size(); i++){
+			cout << global_table[i];
+			if (((i+1) % game_info[COLUMN_SIZE]) == 0){
+				cout << endl;
+			}
+		}
+		cout << endl;
     }
 
 	// tell how many steps, rows and colums we have 
@@ -172,13 +224,6 @@ int main(int argc, char *argv[]) {
 	// local vectors
 	vector<int> local(chunk_size[rank]);
 		
-	for (int i=0; i < global_table.size(); i++){
-		cout << global_table[i];
-		if (((i+1) % game_info[COLUMN_SIZE]) == 0){
-			cout << endl;
-		}
-	}
-	cout << endl;
 
 	// scatter the global board among ranks using sendCounts and displacements
     MPI_Scatterv(global_table.data(), chunk_size.data(), indexes.data(), MPI_INT,  
@@ -186,18 +231,20 @@ int main(int argc, char *argv[]) {
 
 	game_of_life(&local, game_info, rank, size);
 
-
 	// reasembly
 	MPI_Gatherv(local.data(), chunk_size[rank], MPI_INT, global_table.data(),
 		chunk_size.data(), indexes.data(), MPI_INT, MAIN_PROCES, MPI_COMM_WORLD);
 
-	for (int i=0; i < global_table.size(); i++){
-		cout << global_table[i];
-		if (((i+1) % game_info[COLUMN_SIZE]) == 0){
-			cout << endl;
+	// end 
+	if (rank == MAIN_PROCES){
+		cout << "END: " << endl;
+		for (int i=0; i < global_table.size(); i++){
+			cout << global_table[i];
+			if (((i+1) % game_info[COLUMN_SIZE]) == 0){
+				cout << endl;
+			}
 		}
 	}
-	cout << endl;
 
 
     MPI_Finalize();
